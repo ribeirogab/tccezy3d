6443e020f80fd96519784048d0a63e5656c2943a
Gabriel de Oliveira <gblosr@gmail.com>
2019-05-04 17:55:36 -0300

    correção de erro na classe Today.php

--- dashboard/Classes/Today.php
+++ dashboard/Classes/Today.php
@@ -11,20 +11,23 @@ class Today extends Data{
 	public function getTipo(){return $this->tipo;}
 	public function getTotal(){return $this->total;}
 
-    public function setTipo($tipo){$this->tipo = $tipo;}
-    public function setTotal($total){$this->total = $total;}
+	public function setTipo($tipo){$this->tipo = $tipo;}
+	public function setTotal($total){$this->total = $total;}
 
-	public function total($tipo){
-		$valores["dia"] = $this->getDia();
-		$valores["mes"] = $this->getMes();
-		$valores["ano"] = $this->getAno();
+	public function total($tipo){	
 		if($tipo === "visitantes"){
+			$valores["dia"] = $this->getDia();
+			$valores["mes"] = $this->getMes();
+			$valores["ano"] = $this->getAno();
 			$sql = "SELECT SUM(cliques) FROM today WHERE (dia=:dia AND fkmes=:mes AND fkano=:ano) 
 			AND (fktipo='facebook' OR fktipo='instagram' OR fktipo='googleads' OR fktipo='browser')";	
 			$dados = parent::executarFetchAll($sql, $valores);
 		}
 		else{
 			$valores["tipo"] = $tipo;
+			$valores["dia"] = $this->getDia();
+			$valores["mes"] = $this->getMes();
+			$valores["ano"] = $this->getAno();
 			$sql = "SELECT cliques FROM today WHERE (fktipo=:tipo) AND (dia=:dia AND fkmes=:mes AND fkano=:ano)";
 			$dados = parent::executarFetchAll($sql, $valores);
 		}
